dda38f7217358bff5cf28e52f3dd59c869598492
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-07-22 00:36:57 +0700

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -42,7 +42,7 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
         
-        x = random.randint(1,3)
+        x = random.randint(1,4)
         if x == 1:
             progress = "[{0}{1}] \nP: {2}%\n".format(
                 ''.join(["🌳" for i in range(math.floor(percentage / 5))]),
@@ -50,13 +50,18 @@ async def progress_for_pyrogram(
                 round(percentage, 2))
         if x == 2:
             progress = "[{0}{1}] \nP: {2}%\n".format(
-                ''.join(["🫀" for i in range(math.floor(percentage / 5))]),
-                ''.join(["🧠" for i in range(20 - math.floor(percentage / 5))]),
+                ''.join(["🌧" for i in range(math.floor(percentage / 5))]),
+                ''.join(["☁️" for i in range(20 - math.floor(percentage / 5))]),
                 round(percentage, 2))
         if x == 3:
             progress = "[{0}{1}] \nP: {2}%\n".format(
                 ''.join(["🍂" for i in range(math.floor(percentage / 5))]),
                 ''.join(["🍃" for i in range(20 - math.floor(percentage / 5))]),
+                round(percentage, 2))
+        if x == 4:
+            progress = "[{0}{1}] \nP: {2}%\n".format(
+                ''.join(["🐳" for i in range(math.floor(percentage / 5))]),
+                ''.join(["🐋" for i in range(20 - math.floor(percentage / 5))]),
                 round(percentage, 2))
         tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
             humanbytes(current),
