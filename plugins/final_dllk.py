cbdc6d58e684f15a510748b4b5d5d0bcccd6f32b
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-07-23 21:00:55 +0700

    Update final_dllk.py

--- plugins/final_dllk.py
+++ plugins/final_dllk.py
@@ -10,26 +10,26 @@ import os
 import re
 
 @pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*http.*"))
-async def trans(bot, update):
+def trans(bot, update):
     if update.from_user.id in Config.AUTH_USERS:
         web_url = update.text
         Trnl.sh1.update('M3',web_url)
         if "https://goldchannel.net/movies/" in web_url:
-            gdrv_lk = await gldchnl(web_url)
+            gdrv_lk = gldchnl(web_url)
             #await bot.send_message(
                 #chat_id=update.chat.id,
                 #text=gdrv_lk
             #)
             base = Trnl.sh1.acell('K2').value
-            final_link = await transloader(base,gdrv_lk)
+            final_link = transloader(base,gdrv_lk)
             Trnl.sh1.update('L3', final_link)
-            await bot.send_message(
+            bot.send_message(
                 chat_id=update.chat.id,
                 text="Link မှန်ကန်ပါက ဇာတ်ကားတင်လို့ရပါပြီ 👇\n" + final_link
             )
         if "https://channelmyanmar.org/" in web_url:
-            ytsn_lk = await cnmm(web_url)
-            gdrv_lk = await ytsn_dllk(ytsn_lk)
+            ytsn_lk = cnmm(web_url)
+            gdrv_lk = ytsn_dllk(ytsn_lk)
             #await bot.send_message(
                 #chat_id=update.chat.id,
                 #text=gdrv_lk
@@ -37,7 +37,7 @@ async def trans(bot, update):
             base = Trnl.sh1.acell('K2').value
             final_link = await transloader(base,gdrv_lk)
             Trnl.sh1.update('L3', final_link)
-            await bot.send_message(
+            bot.send_message(
                 chat_id=update.chat.id,
                 text="Link မှန်ကန်ပါက ဇာတ်ကားတင်လို့ရပါပြီ 👇\n" + final_link
             )
