cd8963118cc54794057ee4894b203731d6dfb9a1
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-07-23 16:36:15 +0700

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -44,17 +44,5 @@ async def start(bot, update):
         # logger.info(update)
         await bot.send_message(
             chat_id=update.chat.id,
-            text=Translation.START_TEXT.format(update.from_user.first_name),
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            "Source Code", url="https://github.com/MMKZA/filmdaybot.git"
-                        ),
-                        InlineKeyboardButton("Channel", url="https://t.me/filmdayfd"),
-                    ],
-                    [InlineKeyboardButton("Group", url="https://t.me/filmdaychat")],
-                ]
-            ),
-            reply_to_message_id=update.message_id
+            text=Translation.START_TEXT.format(update.from_user.first_name)
         )
