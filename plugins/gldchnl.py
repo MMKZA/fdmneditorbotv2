25c76583c6a8382ec2e222ab1a95c06e82a659da
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-07-25 02:23:21 +0700

    Update gldchnl.py

--- plugins/gldchnl.py
+++ plugins/gldchnl.py
@@ -12,82 +12,104 @@ def gldchnl(gld_url):
     for a in soup.find_all('a', href=True):
         urls_lst.append(a['href'])
     url_lst = [x for x in urls_lst if x.startswith('https://goldchannel.net/links/')]
-    sizes_lst = []
+    qlt_lst = []
     for a in soup.find_all("strong", {"class": "quality"}):
-        sizes_lst.append(a.text)
+        qlt_lst.append(a.text)
+    sz_lst = []
     table = ''
     for a in soup.find_all('div', {'class': 'links_table'}):
         table = a.text
-    sttrm1080 = 'G Drive FHD 1080pMyanmar'
-    sttrm720 = 'G Drive HD 720pMyanmar'
-    sttrm480 = 'G Drive SD 480pMyanmar'
-    entrmGB = ' GB'
-    entrmMB = ' MB'
+    sttrm1080_lst = ['G Drive FHD 1080pMyanmar', 'Mega FHD 1080pMyanmar']
+    sttrm720_lst = ['G Drive HD 720pMyanmar', 'Mega HD 720pMyanmar']
+    sttrm480_lst = ['G Drive SD 480pMyanmar', 'Mega SD 480pMyanmar']
+    gb_lst = ['GB', 'Gb', 'gb', 'gB']
+    mb_lst = ['MB', 'Mb', 'mb', 'mB']
+    for x in gb_lst:
+        if x in table:
+            entrmGB = x
+    for x in mb_lst:
+        if x in table:
+            entrmMB = x
     sz_lst = []
-    if sttrm1080 in table:
-        if entrmGB in table:
-            size1080 = (table.split(sttrm1080))[1].split(entrmGB)[0]
+    for k in sttrm1080_lst:
+        if k in table:
+            if entrmGB in table:
+                size1080 = (table.split(k))[1].split(entrmGB)[0]
             if "Myanmar" in size1080:
-                size1080 = "{:.2f}".format(float((table.split(sttrm1080))[1].split(entrmMB)[0]) / 1024)
+                size1080 = float("{:.2f}".format(float((table.split(k))[1].split(entrmMB)[0]) / 1024))
             else:
-                size1080 = "{:.2f}".format(float(size1080))
-            sz_lst.append(float(size1080))
-    if sttrm720 in table:
-        if entrmGB in table:
-            size720 = (table.split(sttrm720))[1].split(entrmGB)[0]
+                size1080 = float(size1080)
+            sz_lst.append(size1080)
+
+    for k in sttrm720_lst:
+        if k in table:
+            if entrmGB in table:
+                size720 = (table.split(k))[1].split(entrmGB)[0]
             if "Myanmar" in size720:
-                size720 = "{:.2f}".format(float((table.split(sttrm720))[1].split(entrmMB)[0]) / 1024)
+                size720 = float("{:.2f}".format(float((table.split(k))[1].split(entrmMB)[0]) / 1024))
             else:
-                size720 = "{:.2f}".format(float(size720))
-            sz_lst.append(float(size720))
-    if sttrm480 in table:
-        size480 = "{:.2f}".format(float((table.split(sttrm480))[1].split(entrmMB)[0]) / 1024)
-        sz_lst.append(float(size480))
-    a = list(range(0, len(sizes_lst)))
-    chs_lst = list(range(0, len(sizes_lst)))
-    chss_lst = list(range(0, len(sizes_lst)))
+                size720 = float(size720)
+            sz_lst.append(size720)
+    for k in sttrm480_lst:
+        if k in table:
+            if entrmGB in table:
+                size480 = (table.split(k))[1].split(entrmGB)[0]
+            if "Myanmar" in size480:
+                size480 = "{:.2f}".format(float((table.split(k))[1].split(entrmMB)[0]) / 1024)
+            else:
+                size480 = float(size480)
+            sz_lst.append(float(size480))
+
+    a = list(range(0, len(qlt_lst)))
+    qlt_url = {}
     for i in a:
-        chs_lst[i] = url_lst[i] + " | " + sizes_lst[i]
-    for i in a:
-        if "1080" in chs_lst[i]:
-            chss_lst[i] = chs_lst[i] + " | " + str(size1080) + "GB"
-        if "720" in chs_lst[i]:
-            chss_lst[i] = chs_lst[i] + " | " + str(size720) + "GB"
-        if "480" in chs_lst[i]:
-            chss_lst[i] = chs_lst[i] + " | " + str(size480) + "GB"
-    gdrv_1080 = ''
-    gdrv_720 = ''
-    gdrv_480 = ''
-    gld_lk = ''
-    sz_qlt = {}
-    sz_lks = {}
-    for a in chss_lst:
-        if "G Drive FHD 1080p" in a:
-            gdrv_1080 = list(filter(lambda x: "G Drive FHD 1080p" in x, chss_lst))
-            lk_1080 = re.search("(?P<url>https?://[^\s]+)", gdrv_1080[0]).group("url")
-            sz_qlt.update({size1080:"G Drive FHD 1080p".replace("G Drive ","")})
-            sz_lks.update({size1080:lk_1080})
-        if "G Drive HD 720p" in a:
-            gdrv_720 = list(filter(lambda x: "G Drive HD 720p" in x, chss_lst))
-            lk_720 = re.search("(?P<url>https?://[^\s]+)", gdrv_720[0]).group("url")
-            sz_qlt.update({size720:"G Drive HD 720p".replace("G Drive ","")})
-            sz_qlt.update({size720:lk_720})
-        if "G Drive SD 480p" in a:
-            gdrv_480 = list(filter(lambda x: "G Drive SD 480p" in x, chss_lst))
-            lk_480 = re.search("(?P<url>https?://[^\s]+)", gdrv_480[0]).group("url")
-            sz_qlt.update({size480:"G Drive SD 480p".replace("G Drive ","")})
-            sz_qlt.update({size480:lk_480})
+        qlt_url.update({qlt_lst[i]: url_lst[i]})
+    qlt1080 = ['G Drive FHD 1080p', 'Mega FHD 1080p']
+    qlt720 = ['G Drive HD 720p', 'Mega HD 720p']
+    qlt480 = ['G Drive SD 4880p', 'Mega SD 480p']
+    qlt_sz = {}
+
+    for m in qlt1080:
+        for y in qlt_lst:
+            if m in y:
+                qlt_sz.update({y: size1080})
+    for m in qlt720:
+        for y in qlt_lst:
+            if m in y:
+                qlt_sz.update({y: size720})
+    for m in qlt480:
+        for y in qlt_lst:
+            if m in y:
+                qlt_sz.update({y: size480})
+    sz_url = {};
+
+    for m in qlt1080:
+        for y in qlt_url:
+            if m in y:
+                sz_url.update({size1080: qlt_url[m]})
+    for m in qlt720:
+        for y in qlt_url:
+            if m in y:
+                sz_url.update({size720: qlt_url[m]})
+    for m in qlt480:
+        for y in qlt_url:
+            if m in y:
+                sz_url.update({size480: qlt_url[m]})
     indices = [v for i, v in enumerate(sz_lst) if v < float(2)]
-    max_sz = "{:.2f}".format(max(indices))
-    max_qlt = sz_qlt[max_sz]
-    Trnl.sh2.update('H2', max_qlt)
-    max_lk = sz_lks[max_sz]
-    gld_req = requests.get(max_lk)
-    gld_req.encoding = gld_req.apparent_encoding
-    gld_html = gld_req.text
-    soup = BeautifulSoup(gld_html, 'html.parser')
-    gld_lsts = []
+    max_sz = max(indices)
+    gdrv_lk = sz_url[max_sz]
+    gdrv_req = requests.get(gdrv_lk)
+    gdrv_req.encoding = gdrv_req.apparent_encoding
+    gdrv_html = gdrv_req.text
+    soup = BeautifulSoup(gdrv_html, 'html.parser')
+    href_lst = []
     for a in soup.find_all('a', href=True):
-        gld_lsts.append(a['href'])
-    gdrv_lk = [x for x in gld_lsts if x.startswith('https://drive.google.com/file/d/')][0]
-    return gdrv_lk
+        href_lst.append(a['href'])
+    startswith_lst = ['https://drive.google.com/file/d/', 'https://mega.nz/file/']
+    dllk_lst = []
+    for h in href_lst:
+        for k in startswith_lst:
+            if k in h:
+                dllk_lst.append([x for x in href_lst if x.startswith(k)][0])
+    dllk = dllk_lst[0]
+    return dllk
