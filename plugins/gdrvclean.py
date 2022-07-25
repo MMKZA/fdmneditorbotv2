4545c050c6bd32745f8d955d65896c18c54498c1
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-07-25 18:11:47 +0700

    Update gdrvclean.py

--- plugins/gdrvclean.py
+++ plugins/gdrvclean.py
@@ -1,12 +1,19 @@
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
+from trnl import Trnl
 import json
 
 def gdrvclean(status):
+    if "tharphyo" in Trnl.sh1.acell('N2').value:
+        my_json = 'token.json'
+    if "st121" in Trnl.sh1.acell('N2').value:
+        my_json = 'aittoken.json'
+    if "robert" in Trnl.sh1.acell('N2').value:
+        my_json = 'roberttoken.json'
     if "error" in status:
         scope = ['https://www.googleapis.com/auth/drive']
-        creds = Credentials.from_authorized_user_file('token.json', scope)
+        creds = Credentials.from_authorized_user_file(my_json, scope)
         service = build('drive', 'v3', credentials=creds)
         page_token = None
         results = service.files().list(q="name contains 'YoteShin'",
