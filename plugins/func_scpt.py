4b33603da5709d7e2e0c00d2b9b0e454bc5b42f9
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-07-27 17:26:42 +0700

    Update func_scpt.py

--- plugins/func_scpt.py
+++ plugins/func_scpt.py
@@ -95,74 +95,81 @@ def func_scpt(script_url):
         vlink = (wscpt.split(start2))[1].split(end2)[0] + '.jpg'
         phto_splt = vlink.split('/')
     elif "channelmyanmar" in script_url:
-        if 'tvshows' in script_url:
-            start1 = 'Synopsis of '
-            end1 = 'Translat'
-        if 'tvshows' not in script_url:
-            start1 = 'Complete cast'
-            end1 = 'Download Nulled Scripts and Plugins'
-        start2 = 'image" src="'
-        end2 = '.jpg'
-        vcap = sscpt.split('\n', 1)[0]
-        vtext = (sscpt.split(start1))[1].split(end1)[0]
-        del_vtext = 'Your browser does not support the video tag.  '
-        if del_vtext in vtext:
-            vtext = vtext.replace(del_vtext, '')
-        vlink = all_lks[0]
-        phto_splt = vlink.split('/')
-    if 'tmdb' in vlink:
-        if "channelmyanmar" in script_url:
-            phto_cd = phto_splt[-1]
-            phto_url = 'https://image.tmdb.org/t/p/original/' + phto_cd
-    elif 'tmdb' not in vlink:
+        bd_lks = []
+        bd_soup = soup.select('#cap1 > p')
+        for all in bd_soup:
+            bd_lks.append(all.text)
+        if len(bd_lks) != 0:
+            vtext = "\n".join([str(txt) for txt in bd_lks])
+        else:
+            if 'tvshows' in script_url:
+                start1 = 'Synopsis of '
+                end1 = 'Translat'
+            if 'tvshows' not in script_url:
+                start1 = 'Complete cast'
+                end1 = 'Download Nulled Scripts and Plugins'
+            start2 = 'image" src="'
+            end2 = '.jpg'
+            vcap = sscpt.split('\n', 1)[0]
+            vtext = (sscpt.split(start1))[1].split(end1)[0]
+            del_vtext = 'Your browser does not support the video tag.  '
+            if del_vtext in vtext:
+                vtext = vtext.replace(del_vtext, '')
+            vlink = all_lks[0]
+            phto_splt = vlink.split('/')
+        if 'tmdb' in vlink:
+            if "channelmyanmar" in script_url:
+                phto_cd = phto_splt[-1]
+                phto_url = 'https://image.tmdb.org/t/p/original/' + phto_cd
+        elif 'tmdb' not in vlink:
+            if "goldchannel" in script_url:
+                phto_cd = phto_splt[-1].replace('-200x300', '')
+                phto_url = 'https://image.tmdb.org/t/p/original/' + phto_cd
+            elif "channelmyanmar" in script_url:
+                start3 = 'https://www.imdb.com/title/t'
+                end3 = '/" target="_blank">'
+                start4 = '/mediaviewer/'
+                end4 = '/?ref_=tt_ov_i'
+                start5 = 'https://'
+                end5 = '.jpg'
+                if start3 in wscpt:
+                    imdb_cd = (wscpt.split(start3))[1].split(end3)[0]
+                    imdb_url = start3 + imdb_cd
+                    imdb_req = requests.get(imdb_url)
+                    imdb_req.encoding = imdb_req.apparent_encoding
+                    imdb_html = imdb_req.text
+                    imdb_soup = BeautifulSoup(imdb_html, 'html.parser')
+                    imdb_wscpt = imdb_soup.prettify()
+                    imdb2_cd = (imdb_wscpt.split(start4))[1].split(end4)[0]
+                    imdb2_url = imdb_url + '/mediaviewer/' + imdb2_cd
+                    imdb2_req = requests.get(imdb2_url)
+                    imdb2_req.encoding = imdb2_req.apparent_encoding
+                    imdb2_html = imdb2_req.text
+                    imdb2_soup = BeautifulSoup(imdb2_html, 'html.parser')
+                    imdb2_wscpt = imdb2_soup.prettify()
+                    imdb3_cd = (imdb2_wscpt.split(start5))[1].split(end5)[0]
+                    phto_url = start5 + imdb3_cd + end5
+                elif start3 not in wscpt:
+                    phto_url = vlink
+        else:
+            phto_url = vlink
+        vd_qlt = Trnl.sh2.acell('H2').value
+        Trnl.sh2.update('A2', vcap + " | " + mv_gnr + vd_qlt + "\n\nဇာတ်ညွှန်း 📜\n\n" + vtext)
+        Trnl.sh2.update('C2', phto_url)
+        Trnl.sh2.update('D2', vcap)
+        vcap_hsh = ''.join(e for e in vcap if e.isalnum())
+        if 'ChannelMyanmar' in vcap_hsh:
+            vcap_hsh = vcap_hsh.replace('ChannelMyanmar', '')
+        elif 'GoldChannelMovies' in vcap_hsh:
+            vcap_hsh = vcap_hsh.replace('GoldChannelMovies', '')
+        else:
+            vcap_hsh = vcap_hsh
+        Trnl.sh2.update('E2', vcap_hsh)
         if "goldchannel" in script_url:
-            phto_cd = phto_splt[-1].replace('-200x300', '')
-            phto_url = 'https://image.tmdb.org/t/p/original/' + phto_cd
+            credit = 'Gold Channel Movies'
         elif "channelmyanmar" in script_url:
-            start3 = 'https://www.imdb.com/title/t'
-            end3 = '/" target="_blank">'
-            start4 = '/mediaviewer/'
-            end4 = '/?ref_=tt_ov_i'
-            start5 = 'https://'
-            end5 = '.jpg'
-            if start3 in wscpt:
-                imdb_cd = (wscpt.split(start3))[1].split(end3)[0]
-                imdb_url = start3 + imdb_cd
-                imdb_req = requests.get(imdb_url)
-                imdb_req.encoding = imdb_req.apparent_encoding
-                imdb_html = imdb_req.text
-                imdb_soup = BeautifulSoup(imdb_html, 'html.parser')
-                imdb_wscpt = imdb_soup.prettify()
-                imdb2_cd = (imdb_wscpt.split(start4))[1].split(end4)[0]
-                imdb2_url = imdb_url + '/mediaviewer/' + imdb2_cd
-                imdb2_req = requests.get(imdb2_url)
-                imdb2_req.encoding = imdb2_req.apparent_encoding
-                imdb2_html = imdb2_req.text
-                imdb2_soup = BeautifulSoup(imdb2_html, 'html.parser')
-                imdb2_wscpt = imdb2_soup.prettify()
-                imdb3_cd = (imdb2_wscpt.split(start5))[1].split(end5)[0]
-                phto_url = start5 + imdb3_cd + end5
-            elif start3 not in wscpt:
-                phto_url = vlink
-    else:
-        phto_url = vlink
-    vd_qlt = Trnl.sh2.acell('H2').value
-    Trnl.sh2.update('A2', vcap + " | " + mv_gnr + vd_qlt + "\n\nဇာတ်ညွှန်း 📜\n\n" + vtext)
-    Trnl.sh2.update('C2', phto_url)
-    Trnl.sh2.update('D2', vcap)
-    vcap_hsh = ''.join(e for e in vcap if e.isalnum())
-    if 'ChannelMyanmar' in vcap_hsh:
-        vcap_hsh = vcap_hsh.replace('ChannelMyanmar', '')
-    elif 'GoldChannelMovies' in vcap_hsh:
-        vcap_hsh = vcap_hsh.replace('GoldChannelMovies', '')
-    else:
-        vcap_hsh = vcap_hsh
-    Trnl.sh2.update('E2', vcap_hsh)
-    if "goldchannel" in script_url:
-        credit = 'Gold Channel Movies'
-    elif "channelmyanmar" in script_url:
-        credit = 'Channel Myanmar'
-    Trnl.sh2.update('F2', credit)
-    msg_whl = phto_url + "\n\n" + vcap + "\n\nဇာတ်ညွှန်း 📜\n\n" + vtext
-    msg_trm = msg_whl[0:4095]
-    Trnl.sh2.update('O2', msg_trm)
+            credit = 'Channel Myanmar'
+        Trnl.sh2.update('F2', credit)
+        msg_whl = phto_url + "\n\n" + vcap + "\n\nဇာတ်ညွှန်း 📜\n\n" + vtext
+        msg_trm = msg_whl[0:4095]
+        Trnl.sh2.update('O2', msg_trm)
