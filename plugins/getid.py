365762089a3bbb445d867e5d7cdd98b12f236351
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-07-28 12:48:41 +0700

    Update getid.py

--- plugins/getid.py
+++ plugins/getid.py
@@ -5,11 +5,8 @@ from trnl import Trnl
 def getid2(bot, update):
     full_id = update.chat.id
     invt_lk = bot.export_chat_invite_link(chat_id=full_id)
-    invt_1st = invt_lk.split('/',1)[0] + "/"
-    invt_2nd = invt_lk.split('/',1)[1]
     Trnl.sh2.update('J2',full_id)
-    Trnl.sh2.update('I2',invt_1st)
-    Trnl.sh2.update('P2',invt_2nd)
+    Trnl.sh2.update('I2',invt_lk)
     bot.delete_messages(
         chat_id=full_id,
         message_ids=update.message_id
