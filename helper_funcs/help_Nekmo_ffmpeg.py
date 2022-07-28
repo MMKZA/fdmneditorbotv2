9b7422629ecf5c8b9f1917ee98d33c94f2754806
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-07-29 02:13:01 +0700

    Update help_Nekmo_ffmpeg.py

--- helper_funcs/help_Nekmo_ffmpeg.py
+++ helper_funcs/help_Nekmo_ffmpeg.py
@@ -132,29 +132,29 @@ async def cult_small_video(video_file, output_directory, start_time, end_time):
         return None
 
 
-async def generate_screen_shots(
-    video_file,
-    output_directory,
-    is_watermarkable,
-    wf,
-    min_duration,
-    no_of_photos
-):
-    metadata = extractMetadata(createParser(video_file))
-    duration = 0
-    if metadata is not None:
-        if metadata.has("duration"):
-            duration = metadata.get('duration').seconds
-    if duration > min_duration:
-        images = []
-        ttl_step = duration // no_of_photos
-        current_ttl = ttl_step
-        for looper in range(0, no_of_photos):
-            ss_img = await take_screen_shot(video_file, output_directory, current_ttl)
-            current_ttl = current_ttl + ttl_step
-            if is_watermarkable:
-                ss_img = await place_water_mark(ss_img, output_directory + "/" + str(time.time()) + ".jpg", wf)
-            images.append(ss_img)
-        return images
-    else:
-        return None
+#async def generate_screen_shots(
+    #video_file,
+    #output_directory,
+    #is_watermarkable,
+    #wf,
+    #min_duration,
+    #no_of_photos
+#):
+    #metadata = extractMetadata(createParser(video_file))
+    #duration = 0
+    #if metadata is not None:
+        #if metadata.has("duration"):
+            #duration = metadata.get('duration').seconds
+    #if duration > min_duration:
+        #images = []
+        #ttl_step = duration // no_of_photos
+        #current_ttl = ttl_step
+        #for looper in range(0, no_of_photos):
+            #ss_img = await take_screen_shot(video_file, output_directory, current_ttl)
+            #current_ttl = current_ttl + ttl_step
+            #if is_watermarkable:
+                #ss_img = await place_water_mark(ss_img, output_directory + "/" + str(time.time()) + ".jpg", wf)
+            #images.append(ss_img)
+        #return images
+    #else:
+        #return None
