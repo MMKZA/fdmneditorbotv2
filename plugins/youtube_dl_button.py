9b003cecc27abf835d958c558d20d71195920bb8
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-07-29 02:12:12 +0700

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -206,15 +206,15 @@ async def youtube_dl_call_back(bot, update):
                 message_id=update.message.message_id
             )
         else:
-            is_w_f = False
-            images = await generate_screen_shots(
-            download_directory,
-            tmp_directory_for_each_user,
-            is_w_f,
-            Config.DEF_WATER_MARK_FILE,
-            300,
-            9
-            )
+            #is_w_f = False
+            #images = await generate_screen_shots(
+            #download_directory,
+            #tmp_directory_for_each_user,
+            #is_w_f,
+            #Config.DEF_WATER_MARK_FILE,
+            #300,
+            #9
+            #)
             #logger.info(images)
             await bot.edit_message_text(
                 text=Translation.UPLOAD_START,
