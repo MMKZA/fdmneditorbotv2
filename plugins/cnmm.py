2963deaa00a00685dcbff4282aedc17d3569fa69
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-07-31 04:03:31 +0700

    Update cnmm.py

--- plugins/cnmm.py
+++ plugins/cnmm.py
@@ -38,46 +38,52 @@ def cnmm(web_url):
             #sz_lst.append("0 GB")
         #else:
             #sz_lst.append(s)
-    gb_lst = ['GB', 'Gb', 'gb' 'gB']
-    mb_lst = ['MB', 'Mb', 'mb' 'mB']
-    szgb_lst = []
-    for v in sz_lst:
-        szspl = re.findall('(\d+|[A-Za-z]+)', v)
-        if len(szspl) != 0:
-            szunt = szspl[len(szspl) - 1]
-            for g in gb_lst:
-                if szunt in g:
-                    szgb_lst.append(float("{:.2f}".format(float(v.replace(szunt, "").strip()))))
-            for m in mb_lst:
-                if szunt in m:
-                    szgb_lst.append(float("{:.2f}".format(float(v.replace(szunt, "").strip()) / 1024)))
-    all_lst = list(range(0, len(szgb_lst)))
-    for i in all_lst:
-        all_lst[i] = ("{}|{}|{}".format(url_lst[i], qlt_lst[i], str(szgb_lst[i]) + "GB"))
-    indices = [v for i, v in enumerate(szgb_lst) if v < 2]
-    max_sz = float("{:.2f}".format(max(indices)))
-    max_lst = list(filter(lambda x: str(max_sz) + "GB" in x, all_lst))
-    cnmm_lst = []
-    kwd_st = ['https://vip.yoteshinportal.cc/', 'https://yoteshinportal.cc/', 'https://mega.nz/file/']
-    for k in kwd_st:
+    try:
+        gb_lst = ['GB', 'Gb', 'gb' 'gB']
+        mb_lst = ['MB', 'Mb', 'mb' 'mB']
+        szgb_lst = []
+        for v in sz_lst:
+            szspl = re.findall('(\d+|[A-Za-z]+)', v)
+            if len(szspl) != 0:
+                szunt = szspl[len(szspl) - 1]
+                for g in gb_lst:
+                    if szunt in g:
+                        szgb_lst.append(float("{:.2f}".format(float(v.replace(szunt, "").strip()))))
+                for m in mb_lst:
+                    if szunt in m:
+                        szgb_lst.append(float("{:.2f}".format(float(v.replace(szunt, "").strip()) / 1024)))
+        all_lst = list(range(0, len(szgb_lst)))
+        for i in all_lst:
+            all_lst[i] = ("{}|{}|{}".format(url_lst[i], qlt_lst[i], str(szgb_lst[i]) + "GB"))
+        indices = [v for i, v in enumerate(szgb_lst) if v < 2]
+        max_sz = float("{:.2f}".format(max(indices)))
+        max_lst = list(filter(lambda x: str(max_sz) + "GB" in x, all_lst))
+        cnmm_lst = []
+        kwd_st = ['https://vip.yoteshinportal.cc/', 'https://yoteshinportal.cc/', 'https://mega.nz/file/']
+        for k in kwd_st:
+            for m in max_lst:
+                if k in m:
+                    cnmm_lst.append(m.split("|", 3)[0])
+        prr_cnmm = cnmm_lst[0]
+        max_lk = prr_cnmm.split("|", 3)[0]
+        qlt_kwd = []
+        max_qlt = ""
+        for k in qlt_lst:
+            if "" != k:
+                qlt_kwd.append(k)
         for m in max_lst:
-            if k in m:
-                cnmm_lst.append(m.split("|", 3)[0])
-    prr_cnmm = cnmm_lst[0]
-    max_lk = prr_cnmm.split("|", 3)[0]
-    qlt_kwd = []
-    max_qlt = ""
-    for k in qlt_lst:
-        if "" != k:
-            qlt_kwd.append(k)
-    for m in max_lst:
-        for q in qlt_kwd:
-            if q in m:
-                max_qlt = m.split("|", 3)[1]
-    if max_qlt == "":
-        max_qlt = "HD"
-    avlb_lk = '\n'.join([str(lk) for lk in cnmm_lst])
-    Trnl.sh2.update('Q2', avlb_lk)
-    Trnl.sh2.update('H2', max_qlt)
-    ytsn_lk = max_lk
+            for q in qlt_kwd:
+                if q in m:
+                    max_qlt = m.split("|", 3)[1]
+        if max_qlt == "":
+            max_qlt = "HD"
+        avlb_lk = '\n'.join([str(lk) for lk in cnmm_lst])
+        Trnl.sh2.update('Q2', avlb_lk)
+        Trnl.sh2.update('H2', max_qlt)
+        ytsn_lk = max_lk
+    except:
+        all_lst = list(range(0, len(sz_lst)))
+        for i in all_lst:
+            all_lst[i] = ("{}|{}|{}".format(url_lst[i], qlt_lst[i], str(sz_lst[i])))
+            ytsn_lk = 'အခက်အခဲဖြစ်ပေါ်နေလို့ Manual ရွေးပါ\n' + "\n".join([str(lk) for lk in all_lst])
     return ytsn_lk
