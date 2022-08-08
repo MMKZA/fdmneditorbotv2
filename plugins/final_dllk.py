8c8da5751c569e4ecf2c8249162dc22fbec35c43
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-08-09 02:39:41 +0700

    Update final_dllk.py

--- plugins/final_dllk.py
+++ plugins/final_dllk.py
@@ -88,6 +88,8 @@ def trans(bot, update):
                 gdrv_lk = lk
             elif "https://drive.google.com/" in lk:
                 gdrv_lk = lk
+            elif "burmesesubtitles.com" in lk:
+                gdrv_lk = lk
             base = Trnl.sh1.acell('K2').value
             final_link = transloader(base, gdrv_lk)
             Trnl.sh2.update('L2', final_link)
