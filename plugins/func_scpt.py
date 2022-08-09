2b7518e91797214bb02756c5b4ab8523e0f1e768
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-08-09 16:15:51 +0700

    Update func_scpt.py

--- plugins/func_scpt.py
+++ plugins/func_scpt.py
@@ -8,6 +8,8 @@ import urllib
 import datetime
 from tmdbv3api import TMDb, Search, Genre
 from json2html import *
+from lxml import html
+from translation import Translation
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -551,79 +553,25 @@ def func_scpt(script_url):
         all_lks = []
         for all in soup.select('div > img'):
             all_lks.append(all['src'])
-        ctry_lst = ['Afghanistan', 'Aland Islands', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola',
-                    'Anguilla',
-                    'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria',
-                    'Azerbaijan',
-                    'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda',
-                    'Bhutan', 'Bolivia, Plurinational State of', 'Bonaire, Sint Eustatius and Saba',
-                    'Bosnia and Herzegovina',
-                    'Botswana', 'Bouvet Island', 'Brazil', 'British Indian Ocean Territory', 'Brunei Darussalam',
-                    'Bulgaria',
-                    'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands',
-                    'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands',
-                    'Colombia', 'Comoros', 'Congo', 'Congo, The Democratic Republic of the', 'Cook Islands',
-                    'Costa Rica',
-                    "Côte d'Ivoire", 'Croatia', 'Cuba', 'Curaçao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti',
-                    'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea',
-                    'Estonia', 'Ethiopia', 'Falkland Islands (Malvinas)', 'Faroe Islands', 'Fiji', 'Finland', 'France',
-                    'French Guiana', 'French Polynesia', 'French Southern Territories', 'Gabon', 'Gambia', 'Georgia',
-                    'Germany',
-                    'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala',
-                    'Guernsey',
-                    'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard Island and McDonald Islands',
-                    'Holy See (Vatican City State)', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India',
-                    'Indonesia',
-                    'Iran, Islamic Republic of', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica',
-                    'Japan',
-                    'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', "Korea, Democratic People's Republic of",
-                    'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic", 'Latvia',
-                    'Lebanon',
-                    'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macao',
-                    'Macedonia, Republic of', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta',
-                    'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico',
-                    'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia', 'Montenegro',
-                    'Montserrat',
-                    'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia',
-                    'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island',
-                    'Northern Mariana Islands',
-                    'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestinian Territory, Occupied', 'Panama',
-                    'Papua New Guinea',
-                    'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar',
-                    'Réunion',
-                    'Romania', 'Russian Federation', 'Rwanda', 'Saint Barthélemy',
-                    'Saint Helena, Ascension and Tristan da Cunha', 'Saint Kitts and Nevis', 'Saint Lucia',
-                    'Saint Martin (French part)', 'Saint Pierre and Miquelon', 'Saint Vincent and the Grenadines',
-                    'Samoa',
-                    'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles',
-                    'Sierra Leone',
-                    'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia',
-                    'South Africa', 'South Georgia and the South Sandwich Islands', 'Spain', 'Sri Lanka', 'Sudan',
-                    'Suriname',
-                    'South Sudan', 'Svalbard and Jan Mayen', 'Swaziland', 'Sweden', 'Switzerland',
-                    'Syrian Arab Republic',
-                    'Taiwan, Province of China', 'Tajikistan', 'Tanzania, United Republic of', 'Thailand',
-                    'Timor-Leste',
-                    'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan',
-                    'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom',
-                    'United States', 'United States Minor Outlying Islands', 'Uruguay', 'Uzbekistan', 'Vanuatu',
-                    'Venezuela, Bolivarian Republic of', 'Viet Nam', 'Virgin Islands, British', 'Virgin Islands, U.S.',
-                    'Wallis and Futuna', 'Yemen', 'Zambia', 'Zimbabwe']
-        ctry_all = []
-        for c in soup.select('p', {'class': 'icon-network'}):
-            ctry_all.append(c.text)
-        if len(ctry_all) != 0:
-            for x in ctry_all:
-                for y in ctry_lst:
-                    if y in x:
-                        mv_ctry = y
-                        if "India" in mv_ctry:
-                            Trnl.sh2.update('J2', '-1001718578294')
-                            Trnl.sh2.update('I2', 'https://t.me/c/1718578294/')
+        ctry_lst = Translation.ctry_lst
+        ctry = ''
         try:
             ctry = omdb_req['Country']
         except:
+            tree = html.fromstring(req.content)
+            xpth_ctry = tree.xpath('//*[@id="uwee"]/div[2]/p[4]/text()')
+            mv_ctry = []
+            if len(xpth_ctry) != 0:
+                for x in xpth_ctry:
+                    for y in ctry_lst:
+                        if y in x:
+                            mv_ctry.append(y)
+            ctry = ", ".join(c for c in mv_ctry)
+        if ctry == '':
             ctry = '⁉'
+        if "India" in ctry:
+            Trnl.sh2.update('J2', '-1001718578294')
+            Trnl.sh2.update('I2', 'https://t.me/c/1718578294/')
         bd_lks = []
         bd_soup = soup.select('#cap1 > p')
         for all in bd_soup:
