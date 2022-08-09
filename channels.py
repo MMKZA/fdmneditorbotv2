99eaa253b9ff29277a1d18c9e3f58f53bb161287
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-08-09 20:11:33 +0700

    Create channels.py

---
+++ channels.py
@@ -0,0 +1,17 @@
+class channels(object):
+    ani_chcl = ['-1001389311243',
+                'https://t.me/c/1389311243/',
+                'https://t.me/+iVLlmOtK3fZkYTRl'
+               ]
+    bt_chnl = ['-1001718578294',
+               'https://t.me/c/1718578294/',
+               'https://t.me/+GhjrL9gt5wcyNDll'
+              ]
+    gn_chnl = ['-1001604450849',
+               'https://t.me/c/1604450849/',
+               'https://t.me/+8_dPvbx3301mYjQ1'
+              ]
+    rt_chnl = ['-1001750623132',
+               'https://t.me/c/1750623132/',
+               'https://t.me/+ul0bN92c38o0MzM9'
+              ]
