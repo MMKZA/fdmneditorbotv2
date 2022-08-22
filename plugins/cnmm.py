8bd719acef8b35c8657684aece10b617d6c3bb7a
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-08-23 01:49:31 +0700

    Update cnmm.py

--- plugins/cnmm.py
+++ plugins/cnmm.py
@@ -81,9 +81,11 @@ def cnmm(web_url):
         Trnl.sh2.update('Q2', avlb_lk)
         Trnl.sh2.update('H2', max_qlt)
         ytsn_lk = max_lk
+        ytsn_all = 'Link အားလုံး 👇\n' + "\n".join([str(lk) for lk in all_lst])
+        return [ytsn_lk,ytsn_all]
     except:
         all_lst = list(range(0, len(sz_lst)))
         for i in all_lst:
             all_lst[i] = ("{} | {} | {}".format(url_lst[i], qlt_lst[i], str(sz_lst[i])))
             ytsn_lk = 'အခက်အခဲဖြစ်ပေါ်နေလို့ Manual ရွေးပါ\n' + "\n".join([str(lk) for lk in all_lst])
-    return ytsn_lk
+        return ytsn_lk
