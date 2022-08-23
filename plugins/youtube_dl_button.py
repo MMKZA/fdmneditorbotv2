05508eac36e9cc8e4095c4d5ba39f54c7230c996
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-08-23 18:11:29 +0700

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -173,6 +173,43 @@ async def youtube_dl_call_back(bot, update):
     )
     # Wait for the subprocess to finish
     stdout, stderr = await process.communicate()
+    raw_prog = stdout.decode().strip().replace(" ","")
+    prog_lst = re.findall('\[download][0-9]*\.[0-9]+%of[0-9]*\.[0-9]+[a-zA-Z]+[0-9]*\.[0-9]+[a-zA-Z]+/sETA[0-9]+:[0-9]+', raw_prog)
+    prcnt_lst = []
+    ttl_sz = ''
+    spd_lst = []
+    eta_lst = []
+    spd_kw = ['KiB/s','MiB/s','GiB/s']
+    sz_kw = ['KiBat','MiBat','GiBat']
+    spd_unt = ''
+    sz_unt = ''
+    for p in prog_lst:
+        prcnt_lst.append(re.findall('[0-9]*\.[0-9]+%', p)[0])
+        eta_lst.append(re.findall('ETA[0-9]+:[0-9]+', p)[0].replace('ETA',''))
+        k = re.findall('[0-9]*\.[0-9]+[a-zA-Z]+/s', p)[0]
+        for spd in spd_kw:
+            if spd in k:
+                spd_lst.append(k.replace(spd,''))
+                spd_unt = spd
+        l = re.findall('[0-9]*\.[0-9]+[a-zA-Z]+',p)[0]
+        for sz in sz_kw:
+            if sz in l:
+                ttl_sz = l.replace(sz,'')
+                sz_unt = sz.replace('at','')
+    dld_lst = []
+    for p in prcnt_lst:
+        dld_lst.append("{:.2f}".format(float(p.strip('%'))*float(ttl_sz)/100))
+    for i in range(0,len(prcnt_lst)):
+        text = Translation.DOWNLOAD_START + '\n<code>{}</code>\nPercent Completed: {}\nDownloaded: {} of {} {}\nSpeed: {} {}\nETA: {} min : {} sec'.format(prcnt_lst[i],
+                                                                                                       dld_lst[i],
+                                                                                                       ttl_sz,
+                                                                                                       sz_unt,
+                                                                                                       spd_lst[i],
+                                                                                                       spd_unt,
+                                                                                                       eta_lst[i].split(':')[0],
+                                                                                                       eta_lst[i].split(':')[1]
+                                                                                                       )
+        await a.edit_text(text)
     e_response = stderr.decode().strip()
     t_response = stdout.decode().strip()
     logger.info(e_response)
