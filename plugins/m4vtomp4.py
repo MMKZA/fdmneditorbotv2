a0ee1d25351ca12a24c54d6a7104e43515dd8885
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-08-30 23:02:32 +0700

    Update m4vtomp4.py

--- plugins/m4vtomp4.py
+++ plugins/m4vtomp4.py
@@ -3,18 +3,22 @@ from bs4 import BeautifulSoup
 import urllib
 
 def mp4(url, base):
-    #url = 'https://app.rapidleech.gq/files/Black.Site.2022.HD_GC.m4v'
-    #base = 'https://app.rapidleech.gq/'
-    base_host = base.split('/')[-1]
+    base_host = base.split('/')[-2]
+    print(base_host)
     kwd = urllib.parse.unquote_plus(url.split('/')[-1])
-    nw_nm = kwd.replace('.m4v','.mp4')
-    nw_url = 'https://app.rapidleech.gq/files/' + nw_nm
+    print(kwd)
+    ext_lst = ['.m4v','.mkv']
+    for e in ext_lst:
+        if e in kwd:
+            nw_nm = kwd.replace(e,'.mp4')
+    nw_url = base + 'files/' + nw_nm    
     r = requests.session()
-    res = r.get(base)
+    res = r.get(base + 'index.php',cookies={"_ga":"GA1.2.1231193415.1658745553"})
     soup = BeautifulSoup(res.content, 'html.parser')
     sp1 = soup.find_all('tr',{'title':kwd})
     sp2 = "".join([str(lk) for lk in sp1[0].find_all('input')])
     fl_id = sp2.split('"')[-2]
+    print(fl_id)
     payload = {
         'act': 'rename_go',
         'files[]': fl_id,
@@ -27,13 +31,13 @@ def mp4(url, base):
         'Accept-Language': 'en-US,en;q=0.9',
         'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive',
-        'Content-Length': '76',
+        'Content-Length': '124',
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': '_ga=GA1.2.1231193415.1658745553; showAll=1',
+        'Cookie': '_ga=GA1.2.1231193415.1658745553',
         'Host': base_host,
-        'Origin': base,
+        'Origin': base.replace('/',''),
         'Referer': base + 'index.php',
-        'sec-ch-ua': '" Not;A Brand";v="99", "Microsoft Edge";v="103", "Chromium";v="103"',
+        'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Microsoft Edge";v="104"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
         'Sec-Fetch-Dest': 'document',
@@ -41,7 +45,9 @@ def mp4(url, base):
         'Sec-Fetch-Site': 'same-origin',
         'Sec-Fetch-User': '?1',
         'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.63'
     }
     res = r.post(base,headers=headers,data=payload)
+    print(res)
+    nw_url = base + 'files/' + urllib.parse.quote_plus(nw_nm)
     return nw_url
