add927553c241edce4d6571e8803c0ffa6083046
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-08-31 00:12:35 +0700

    Update transloader.py

--- plugins/transloader.py
+++ plugins/transloader.py
@@ -22,7 +22,7 @@ def transloader(base, last_url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-        "Accept-Encoding": "gzip, deflate",
+        "Accept-Encoding": "utf-8",#"gzip, deflate",
         "Accept-Language": "en-US,en;q=0.9"
     }
     data = dict(
