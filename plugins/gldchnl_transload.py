d421a0e47bb79335e212cd63be40b37510295aa8
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-02 00:21:51 +0700

    Create gldchnl_transload.py

---
+++ plugins/gldchnl_transload.py
@@ -0,0 +1,48 @@
+import pyrogram
+from trnl import Trnl
+import asyncio
+from plugins.gdrvclean import gdrvclean
+from plugins.transloader import transloader
+from plugins.echo_echo import echo_echo
+import os
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+async def cnmm_transload(bot, update):
+    if update.from_user.id in Config.AUTH_USERS:
+        gdrv_lk = update.data
+        logger.info(gdrv_lk)
+        base = Trnl.sh2.acell('K2').value
+        final_link = transloader(base, gdrv_lk)
+        logger.info(final_link)
+        Trnl.sh2.update('L2', final_link)
+        arc_kw = ['.zip','.rar','.7z']
+        vd_kw = ['.mp4','.mkv','.mov','.m4v']
+        fl_ext = os.path.splitext(final_link)[1]
+        if fl_ext in arc_kw:
+            text = "Archive ဖိုင်အမျိုးအစားဖြစ်ပါတယ်၊ 🗃️SFile ကိုရွေးချယ်ပါ 👇\n"
+            mssg = await bot.send_message(
+                chat_id=update.from_user.id,
+                text=text + final_link
+            )
+            await echo_echo(bot,update,final_link,mssg)
+        elif fl_ext in vd_kw:
+            text = "Video ဖိုင်အမျိုးအစားဖြစ်ပါတယ်၊ 📺SVideo ကိုရွေးချယ်ပါ 👇\n"
+            mssg = await bot.send_message(
+                chat_id=update.from_user.id,
+                text=text + final_link
+            )
+            await echo_echo(bot,update,final_link,mssg)
+        else:
+            text = "Link အမှားအယွင်းရှိနိုင်ပါတယ်၊ သေချာစစ်ကြည့်ပါ ⚠️\n"
+            mssg = await bot.send_message(
+                chat_id=update.from_user.id,
+                text=text + final_link
+            )
