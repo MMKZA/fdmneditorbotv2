9816826c7999e9cdd5a5a004e45512fcb6761004
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-02 04:21:23 +0700

    Update cnmm_transload.py

--- plugins/cnmm_transload.py
+++ plugins/cnmm_transload.py
@@ -10,6 +10,7 @@ if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
+    
 import logging
 
 logging.basicConfig(level=logging.DEBUG,
