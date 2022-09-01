14e080f9dbbe23d68baa0f23a6063b55bd2ce58c
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-01 22:25:13 +0700

    Update cnmm_transload.py

--- plugins/cnmm_transload.py
+++ plugins/cnmm_transload.py
@@ -10,19 +10,25 @@ if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
-    
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 async def cnmm_transload(bot, update):
     if update.from_user.id in Config.AUTH_USERS:
         lk = update.data
-        if "yoteshinportal.cc" in lk:
-            gdrv_retrn = ytsn_dllk(lk)
-            if "error" in gdrv_retrn:
-                gdrvclean(gdrv_retrn)
-                gdrv_lk = ytsn_dllk(lk)
-            else:
-                gdrv_lk = gdrv_retrn
+        logger.info(lk)
+        gdrv_retrn = ytsn_dllk(lk)
+        if "error" in gdrv_retrn:
+            gdrvclean(gdrv_retrn)
+            gdrv_lk = ytsn_dllk(lk)
+        else:
+            gdrv_lk = gdrv_retrn
         base = Trnl.sh2.acell('K2').value
         final_link = transloader(base, gdrv_lk)
+        logger.info(final_link)
         Trnl.sh2.update('L2', final_link)
         arc_kw = ['.zip','.rar','.7z']
         vd_kw = ['.mp4','.mkv','.mov','.m4v']
@@ -30,20 +36,20 @@ async def cnmm_transload(bot, update):
         if fl_ext in arc_kw:
             text = "Archive ဖိုင်အမျိုးအစားဖြစ်ပါတယ်၊ 🗃️SFile ကိုရွေးချယ်ပါ 👇\n"
             bot.send_message(
-                chat_id=update.chat.id,
+                chat_id=update.from_user.id,
                 text=text + final_link
             )
             asyncio.run(echo_auto(bot,update,final_link))
         elif fl_ext in vd_kw:
             text = "Video ဖိုင်အမျိုးအစားဖြစ်ပါတယ်၊ 📺SVideo ကိုရွေးချယ်ပါ 👇\n"
             bot.send_message(
-                chat_id=update.chat.id,
+                chat_id=update.from_user.id,
                 text=text + final_link
             )
             asyncio.run(echo_auto(bot,update,final_link))
         else:
             text = "Link အမှားအယွင်းရှိနိုင်ပါတယ်၊ သေချာစစ်ကြည့်ပါ ⚠️\n"
             bot.send_message(
-                chat_id=update.chat.id,
+                chat_id=update.from_user.id,
                 text=text + final_link
             )
