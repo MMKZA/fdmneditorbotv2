c44c92fbf585e38b1920ee12d15ab006a346bc63
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-04 19:50:25 +0700

    Update plhh_gdrive.py

--- plugins/plhh_gdrive.py
+++ plugins/plhh_gdrive.py
@@ -1,14 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 
 
 def plhh_gdrive(gdrv_lk):
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver',chrome_options=options)
+    driver = webdriver.Chrome()
     driver.get("https://publiclinks.hashhackers.com/")
     driveid = driver.find_element(By.XPATH, '//*[@id="driveid"]')
     driveid.click()
