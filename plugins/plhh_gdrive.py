937c4d56cd367a744408ee9cdc46441b943892e9
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-04 15:46:42 +0700

    Create plhh_gdrive.py

---
+++ plugins/plhh_gdrive.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+def plhh_gdrive(gdrv_lk):
+    #gdrv_lk = 'https://drive.google.com/file/d/1c7a1RTj9D0f7cLetIHtcUAZSizkT6iSi/view?usp=sharing'
+    driver = webdriver.Chrome()
+    driver.get("https://publiclinks.hashhackers.com/")
+    driveid = driver.find_element(By.XPATH, '//*[@id="driveid"]')
+    driveid.click()
+    driveid.send_keys(gdrv_lk)
+    convert = driver.find_element(By.XPATH, '//*[@id="encrypt"]')
+    convert.click()
+    result = driver.find_element(By.XPATH, '//*[@id="result"]')
+    my_link = result.get_attribute('value')
+    driver.quit()
+    return my_link
+
