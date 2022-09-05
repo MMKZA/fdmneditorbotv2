bc20279d99cc1c03b09fab7adfa51b4de9f7f437
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-05 18:22:25 +0700

    Update trnl.py

--- trnl.py
+++ trnl.py
@@ -7,7 +7,17 @@ if bool(os.environ.get("WEBHOOK", False)):
 else:
     from config import Config
 import subprocess
+class cd:
+    """Context manager for changing the current working directory"""
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
 
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
 class Trnl(object):
     with open('fdmn-channel-698f5c18ea12.txt','w',encoding = 'utf-8') as f:
         f.write('\n{')
