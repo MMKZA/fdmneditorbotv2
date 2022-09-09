ae031785cd481aec4c61867874b0f9e3ae5c61fc
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-09 23:11:03 +0700

    Update gdrv_id_save.py

--- plugins/gdrv_id_save.py
+++ plugins/gdrv_id_save.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 def cnmm_gdrv_id_save(bot, update):
     if update.from_user.id in Config.AUTH_USERS:
         index = int(update.data.split('|')[1])
-        ytsn_lst_txt = Trnl.sh1.acell('A6').value
+        ytsn_lst_txt = Trnl.sh2.acell('A6').value
         ytsn_lst = ytsn_lst_txt.split('\n')
         logger.info(ytsn_lst)
         lk = ytsn_lst[index].split('|')[0].strip()
@@ -32,11 +32,11 @@ def cnmm_gdrv_id_save(bot, update):
             gdrv_lk = gdrv_retrn
         gdrv_id = gdrv_lk.split('/')[5]
         logger.info(gdrv_id)
-        Trnl.sh1.update('L4',gdrv_id)
+        Trnl.sh2.update('L4',gdrv_id)
         methods(bot,update)
         
 def gldchnl_gdrv_id_save(bot, update):
     if update.from_user.id in Config.AUTH_USERS:
         gdrv_id = update.data.split('|')[1]
-        Trnl.sh1.update('L4',gdrv_id)
+        Trnl.sh2.update('L4',gdrv_id)
         methods(bot,update)
