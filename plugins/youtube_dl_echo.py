15ce1b6c530c0d25191c466406d3217eb32439e6
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-11 14:42:58 +0700

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -218,7 +218,8 @@ async def echo(bot, update):
                 disable_web_page_preview=True
             )
             return False
-        if t_response:
+        t_response = 1
+        if t_response == 1:
             # logger.info(t_response)
             x_reponse = t_response
             if "\n" in x_reponse:
