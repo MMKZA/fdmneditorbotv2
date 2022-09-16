7b0d07f2ca09ea9574d82323abb6eacd30a02088
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-17 01:23:11 +0700

    Update methods.py

--- plugins/methods.py
+++ plugins/methods.py
@@ -46,27 +46,26 @@ def transload_method(bot, update):
         arc_kw = ['.zip','.rar','.7z']
         vd_kw = ['.mp4','.mkv','.mov','.m4v']
         fl_ext = os.path.splitext(final_link)[1]
-        if fl_ext in arc_kw:
-            text = "Archive ဖိုင်အမျိုးအစားဖြစ်ပါတယ်၊ 🗃️SFile ကိုရွေးချယ်ပါ 👇\n"
-            mssg = bot.send_message(
-                chat_id=update.from_user.id,
-                text=text + final_link
-            )
-            echo_echo(bot,update,final_link,mssg,mssg.message_id)
-        elif fl_ext in vd_kw:
-            text = "Video ဖိုင်အမျိုးအစားဖြစ်ပါတယ်၊ 📺SVideo ကိုရွေးချယ်ပါ 👇\n"
-            mssg = bot.send_message(
-                chat_id=update.from_user.id,
-                text=text + final_link
-            )
-            echo_echo(bot,update,final_link,mssg,mssg.message_id)
-        else:
-            text = "Link အမှားအယွင်းရှိနိုင်ပါတယ်၊ သေချာစစ်ကြည့်ပါ ⚠️\n"
-            mssg = bot.send_message(
-                chat_id=update.from_user.id,
-                text=text + final_link
-            )
-
+        if Trnl.sh2.acell('V2').value == 'manual':
+            if fl_ext in arc_kw:
+                text = "Archive ဖိုင်အမျိုးအစားဖြစ်ပါတယ်၊ 🗃️SFile ကိုရွေးချယ်ပါ 👇\n"
+                mssg = bot.send_message(
+                    chat_id=update.from_user.id,
+                    text=text + final_link
+                )
+            elif fl_ext in vd_kw:
+                text = "Video ဖိုင်အမျိုးအစားဖြစ်ပါတယ်၊ 📺SVideo ကိုရွေးချယ်ပါ 👇\n"
+                mssg = bot.send_message(
+                    chat_id=update.from_user.id,
+                    text=text + final_link
+                )
+            else:
+                text = "Link အမှားအယွင်းရှိနိုင်ပါတယ်၊ သေချာစစ်ကြည့်ပါ ⚠️\n"
+                mssg = bot.send_message(
+                    chat_id=update.from_user.id,
+                    text=text + final_link
+                )
+        echo_echo(bot,update,final_link,mssg,mssg.message_id)
 def plhh_method(bot, update):
     if update.from_user.id in Config.AUTH_USERS:
         gdrv_id = Trnl.sh2.acell('L4').value
@@ -83,13 +82,14 @@ def plhh_method(bot, update):
         del req
         final_link = plhh_gdrive(gdrv_lk)
         Trnl.sh2.update('L2', final_link)
-        if 'public.php?' in final_link:
-            text = "📺SVideo or 🗃️SFile မှန်ရာကိုရွေးချယ်ပါ 👇\n"
-            mssg = bot.send_message(
-                chat_id=update.from_user.id,
-                text=text + final_link
-            )
-            echo_echo(bot,update,final_link,mssg,mssg.message_id)
+        if Trnl.sh2.acell('V2').value == 'manual':
+            if 'public.php?' in final_link:
+                text = "📺SVideo or 🗃️SFile မှန်ရာကိုရွေးချယ်ပါ 👇\n"
+                mssg = bot.send_message(
+                    chat_id=update.from_user.id,
+                    text=text + final_link
+                )
+        echo_echo(bot,update,final_link,mssg,mssg.message_id)
             
 def direct_method(bot, update):
     if update.from_user.id in Config.AUTH_USERS:
@@ -107,9 +107,10 @@ def direct_method(bot, update):
         del req
         final_link = gdrv_lk
         Trnl.sh2.update('L2', final_link)
-        text = "📺SVideo or 🗃️SFile မှန်ရာကိုရွေးချယ်ပါ 👇\n"
-        mssg = bot.send_message(
-            chat_id=update.from_user.id,
-            text=text + final_link
-        )
+        if Trnl.sh2.acell('V2').value == 'manual':
+            text = "📺SVideo or 🗃️SFile မှန်ရာကိုရွေးချယ်ပါ 👇\n"
+            mssg = bot.send_message(
+                chat_id=update.from_user.id,
+                text=text + final_link
+            )
         echo_echo(bot,update,final_link,mssg,mssg.message_id)
