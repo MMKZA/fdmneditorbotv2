15e82212912fb44feb306261e7d33170e6e95f94
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-16 18:46:33 +0700

    Update plhh_gdrive.py

--- plugins/plhh_gdrive.py
+++ plugins/plhh_gdrive.py
@@ -2,15 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from pyvirtualdisplay import Display
 
 def plhh_gdrive(gdrv_lk):
-    display = Display(visible=0, size=(800, 800))  
-    display.start()
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
     #gdrv_lk = 'https://drive.google.com/file/d/1c7a1RTj9D0f7cLetIHtcUAZSizkT6iSi/view?usp=sharing'
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get("https://publiclinks.hashhackers.com/")
