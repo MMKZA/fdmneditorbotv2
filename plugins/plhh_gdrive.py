3c36687f87dec619eca075b77279f194dccf9f48
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-16 18:54:11 +0700

    Update plhh_gdrive.py

--- plugins/plhh_gdrive.py
+++ plugins/plhh_gdrive.py
@@ -5,10 +5,12 @@ from selenium.webdriver.chrome.options import Options
 
 def plhh_gdrive(gdrv_lk):
     chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--window-size=1420,1080')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
+    options.addArguments("start-maximized"); // open Browser in maximized mode
+    options.addArguments("disable-infobars"); // disabling infobars
+    options.addArguments("--disable-extensions"); // disabling extensions
+    options.addArguments("--disable-gpu"); // applicable to windows os only
+    options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+    options.addArguments("--no-sandbox"); // Bypass OS security model
     #gdrv_lk = 'https://drive.google.com/file/d/1c7a1RTj9D0f7cLetIHtcUAZSizkT6iSi/view?usp=sharing'
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get("https://publiclinks.hashhackers.com/")
