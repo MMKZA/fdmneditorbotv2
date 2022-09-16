0aac5632d3e8ee620220321b4f47fd6c6f92fc81
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-16 23:54:28 +0700

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -359,7 +359,8 @@ async def echo(bot, update):
             if Trnl.sh2.acell('V2').value == 'auto':
                 arc_kw = ['.zip','.rar','.7z']
                 vd_kw = ['.mp4','.mkv','.mov','.m4v']
-                fl_ext = os.path.splitext(url)[1]
+                fl_fll_nm = Trnl.sh2.acell('D6').value
+                fl_ext = os.path.splitext(fl_fll_nm)[1]
                 if fl_ext in arc_kw or format_ext in arc_kw:
                     Trnl.sh2.update('V2',(cb_string_file).encode("UTF-8"))
                 elif fl_ext in vd_kw or format_ext in vd_kw:
