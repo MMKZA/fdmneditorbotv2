c4eb73c7e0d730120d5b3ebea70c4a68746b8c47
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-17 01:04:20 +0700

    Update plhh_gdrive.py

--- plugins/plhh_gdrive.py
+++ plugins/plhh_gdrive.py
@@ -1,18 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 
 def plhh_gdrive(gdrv_lk):
-    chrome_options = Options()
-    chrome_options.addArguments("start-maximized")
-    chrome_options.addArguments("disable-infobars")
-    chrome_options.addArguments("--disable-extensions")
-    chrome_options.addArguments("--disable-gpu")
-    chrome_options.addArguments("--disable-dev-shm-usage")
-    chrome_options.addArguments("--no-sandbox")
     #gdrv_lk = 'https://drive.google.com/file/d/1c7a1RTj9D0f7cLetIHtcUAZSizkT6iSi/view?usp=sharing'
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver = webdriver.Chrome()
     driver.get("https://publiclinks.hashhackers.com/")
     driveid = driver.find_element(By.XPATH, '//*[@id="driveid"]')
     driveid.click()
