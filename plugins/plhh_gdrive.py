d16841544513588e04a788ceb7f726177fed2045
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-16 18:30:27 +0700

    Update plhh_gdrive.py

--- plugins/plhh_gdrive.py
+++ plugins/plhh_gdrive.py
@@ -2,8 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from pyvirtualdisplay import Display
 
 def plhh_gdrive(gdrv_lk):
+    display = Display(visible=0, size=(800, 800))  
+    display.start()
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
