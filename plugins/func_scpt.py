ef426ed2cbad76eba49cce02ce62e8d7ee3ef69a
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-18 21:45:17 +0700

    Update func_scpt.py

--- plugins/func_scpt.py
+++ plugins/func_scpt.py
@@ -829,6 +829,7 @@ def func_scpt(script_url):
             else:
                 af_lst.append(bf_line)
     vtext = '\n'.join(af_lst)
+    movie_id = str(imdb_id).replace('tt','')
     movie = imdb_data(imdb_id)
     title = movie.data['title']
     kind = movie.data['kind']
