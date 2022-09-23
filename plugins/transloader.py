86374581933d5681ca79caf96e6fc40101d9228e
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-23 15:17:33 +0700

    Update transloader.py

--- plugins/transloader.py
+++ plugins/transloader.py
@@ -8,11 +8,11 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 import logging
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger('chardet.universaldetector').setLevel(logging.INFO)
 
 def transloader(base, last_url):
     req = requests.get(base)
