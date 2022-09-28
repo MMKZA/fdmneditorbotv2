aeed23f2205e6b0982f6eba5da454f152ef94fd0
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-28 14:34:58 +0700

    Create gdtot_dl.py

---
+++ plugins/gdtot_dl.py
@@ -0,0 +1,63 @@
+import re
+import base64
+import requests
+from urllib.parse import urlparse, parse_qs
+
+# gdtot url
+#URL = "https://new.gdtot.cfd/file/318755447" 
+
+# add your crypt cookie here
+crypt = "aHUzZk9HYUJoTUJ1WnZTTlkwQzQ2SkxTbE1lOXJRbEJvTHBVNTF1bE51MD0"
+
+# ==========================================
+
+def parse_info(res):
+    title = re.findall(">(.*?)<\/h5>", res.text)[0]
+    info = re.findall('<td\salign="right">(.*?)<\/td>', res.text)
+    parsed_info = {
+        'error': True,
+        'message': 'Link Invalid.',
+        'title': title,
+        'size': info[0],
+        'date': info[1]
+    }
+    return parsed_info
+
+# ==========================================
+
+def gdtot_dl(url):
+    client = requests.Session()
+    client.cookies.update({ 'crypt': crypt })
+    res = client.get(url)
+
+    info = parse_info(res)
+    info['src_url'] = url
+
+    res = client.get(f"https://new.gdtot.top/dld?id={url.split('/')[-1]}")
+    
+    try:
+        url = re.findall('URL=(.*?)"', res.text)[0]
+    except:
+        info['message'] = 'The requested URL could not be retrieved.',
+        return info
+
+    params = parse_qs(urlparse(url).query)
+    
+    if 'msgx' in params:
+        info['message'] = params['msgx'][0]
+    
+    if 'gd' not in params or not params['gd'] or params['gd'][0] == 'false':
+        return info
+    
+    try:
+        decoded_id = base64.b64decode(str(params['gd'][0])).decode('utf-8')
+        gdrive_url = f'https://drive.google.com/open?id={decoded_id}'
+        info['message'] = 'Success.'
+    except:
+        info['error'] = True
+        return info
+
+    info['error'] = False
+    info['gdrive_link'] = gdrive_url
+    
+    return info
