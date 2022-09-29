f1185b4c58ee78e4950acc7437860decf881f06c
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-09-29 20:14:31 +0700

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -345,6 +345,8 @@ def youtube_dl_call_back(bot, update):
                 except:
                     img = Image.open(ssimg)
                     width,height = img.size
+            Trnl.sh2.update('C5',width)
+            Trnl.sh2.update('C6',height)
             thumb_poster = tmp_directory_for_each_user + '/' + os.path.splitext(vlink.split('/')[-1])[0] + '.jpeg'
             if not os.path.exists(thumb_poster):
                 fdmn_frame(vlink,thumb_poster,width,height)
