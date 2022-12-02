310d0dff23d5f2adf0ea3b9d4994fb5ace4205a8
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-12-02 23:28:35 +0700

    Update plhh_gdrive.py

--- plugins/plhh_gdrive.py
+++ plugins/plhh_gdrive.py
@@ -3,7 +3,11 @@ import json
 
 def plhh_gdrive(gdrv_lk):
     gdrv_id = gdrv_lk.split('/')[5]
-    plr_web = 'https://geolocation.zindex.eu.org/publiclinks.json?id={}'.format(gdrv_id)
+    plr_web = 'https://api.a-u.workers.dev/info/{}?_=1669997773466'.format(gdrv_id)
     req = requests.get(plr_web)
     response = json.loads(req.content)
-    return response['url']
+    url = 'https://api.a-u.workers.dev/download/{}'.format(gdrv_id)
+    if response['name'] is not None:
+        return url#response['url']
+#gdrv_lk = 'https://drive.google.com/file/d/1kAT4BdsylhdPcPK4neP40IxzKrHKZ83M/view?usp=share_link'   
+#print(plhh_gdrive(gdrv_lk))
