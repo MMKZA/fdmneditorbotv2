4f15f0c5bf30feaffa90740a85a8ebad8ccce8a8
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-12-06 14:11:20 +0700

    Update plhh_gdrive.py

--- plugins/plhh_gdrive.py
+++ plugins/plhh_gdrive.py
@@ -4,7 +4,8 @@ import json
 def plhh_gdrive(gdrv_lk):
     gdrv_id = gdrv_lk.split('/')[5]
     plr_web = 'https://api.a-u.workers.dev/info/{}?_=1669997773466'.format(gdrv_id)
-    req = requests.get(plr_web)
+    headers = {'accept-encoding' : 'utf-8'}
+    req = requests.get(plr_web,headers=headers)
     response = json.loads(req.content)
     url = 'https://api.a-u.workers.dev/download/{}'.format(gdrv_id)
     return url#response['url']
